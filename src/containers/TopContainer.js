0e628bab1aa2da07f818a20dc57f809ee4c8c5e1
Umar Muqtadir <um.muq89@gmail.com>
2020-01-15 18:01:02 -0800

    Got carousel to work, need to adjust image size

--- src/containers/TopContainer.js
+++ src/containers/TopContainer.js
@@ -7,43 +7,54 @@ class TopContainer extends Component {
     render() {
         return (
             <Carousel>
-            <Carousel.Item>
-              <img
-                src="https://unsplash.com/photos/1Shk_PkNkNw"
-                alt="First slide"
-              />
-              <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="https://unsplash.com/photos/MNtag_eXMKw"
-                alt="Third slide"
-              />
-    
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="holder.js/800x400?text=Third slide&bg=20232a"
-                alt="Third slide"
-              />
-    
-              <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-        </Carousel>
+                <Carousel.Item>
+                <img
+                    style = {carouselImg}
+                    src="https://i.imgur.com/3ccsTEQ.jpeg"
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                <img
+                    style = {carouselImg}
+                    className="carousel-img"
+                    src="https://i.imgur.com/ZINIWos.jpeg"
+                    alt="Second slide"
+                />
+        
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                <img
+                    style = {carouselImg}
+                    className="carousel-img"
+                    src="https://i.imgur.com/eTuCPxM.jpeg"
+                    alt="Third slide"
+                />
+        
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         )
     }
+}
+
+const carouselImg = {
+    height: 'auto',
+    maxWidth: '100%',
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto'
 }
 
 export default TopContainer
